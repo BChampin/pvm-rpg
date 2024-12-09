dcfede8fa0639740a4ecfbf06cf537d906ea4f22
BChampin <github@champin.dev>
2024-12-09 15:59:01 +0100

    [code] Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/pvm-rpg',
+  assetPrefix: '/pvm-rpg/',
   output: 'export',
 };
 
