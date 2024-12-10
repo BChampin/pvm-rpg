22c5d7839d2814f2fa56a4f6b0686090b6f2880b
BChampin <github@champin.dev>
2024-12-10 23:23:36 +0100

    [script] Update generate to handle players and timeRecords, clean public folder

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -24,12 +24,27 @@ function getMapGrade(letter) {
   return mapGrade
 }
 
-function writeData(file, data) {
-  // Define the output path
-  const outputPath = `./public/${file}.json`
+function readData(file) {
+  return JSON.parse(fs.readFileSync(`./public/data/${file}.json`))
+}
 
-  // Write the formatted data to a JSON file
-  fs.writeFileSync(outputPath, JSON.stringify(data, null, 2))
+function writeData(file, data) {
+  fs.writeFileSync(`./public/data/${file}.json`, JSON.stringify(data))
+}
+
+
+function uniqueStr () {
+  return `${Date.now().toString(36)}_${Math.random().toString(36).substring(2, 10)}`
+}
+
+function getMapFromName (arr, name) {
+  return arr.find(item => item.label === name)
+}
+
+function timeStrToNumber (time) {
+  const [minutes, seconds] = time.split(':')
+  const [sec, millis] = seconds.split('.')
+  return parseInt(minutes) * 60 * 1000 + parseInt(sec) * 1000 + parseInt(millis)
 }
 
 // ## Main functions
@@ -51,7 +66,6 @@ async function generateMaps() {
   try {
     const alienJson = await fetchSheetData('PvM (Aliens)')
     const noviceJson = await fetchSheetData('PvM (Novice)')
-    // const maps = []
 
     const mapsPromises = alienJson.table.rows[2].c.map(async (cell, cellIndex) => {
       if (cellIndex <= 1 || !cell.v) return null
@@ -71,7 +85,7 @@ async function generateMaps() {
         exchange.link = link
         const match = link.match(new RegExp(/\/maps\/(\d+)\//))
         if (match && match[1]) {
-          const id = match[1]
+          const id = Number(match[1])
           exchange.id = id
           const authorRequest = await fetch(`https://trackmania.exchange/api/maps/get_authors/${id}`)
           const authorJson = await authorRequest.json()
@@ -85,10 +99,10 @@ async function generateMaps() {
         label: cell.v,
         grade: mapGrade ?? undefined,
         times: {
-          alien: alienJson.table.rows[4].c[cellIndex]?.v ?? 'not found',
-          player: alienJson.table.rows[3].c[cellIndex]?.v ?? 'not found',
-          intermediate: noviceJson.table.rows[4].c[cellIndex]?.v ?? 'not found',
-          noob: noviceJson.table.rows[3].c[cellIndex]?.v ?? 'not found',
+          alien: timeStrToNumber(alienJson.table.rows[4].c[cellIndex]?.v),
+          player: timeStrToNumber(alienJson.table.rows[3].c[cellIndex]?.v),
+          intermediate: timeStrToNumber(noviceJson.table.rows[4].c[cellIndex]?.v),
+          noob: timeStrToNumber(noviceJson.table.rows[3].c[cellIndex]?.v),
         },
         exchange
       }
@@ -101,6 +115,104 @@ async function generateMaps() {
   }
 }
 
+async function generatePlayers() {
+  try {
+    // Gather maps
+    const mapsIdFromCol = {}
+    const maps = readData('maps')
+    const alienJson = await fetchSheetData('PvM (Aliens)')
+    for (const [mapColIndex, mapName] of alienJson.table.rows[2].c.entries()) {
+      if (mapName && mapName.v) {
+        const map = getMapFromName(maps, mapName.v)
+        if (map) mapsIdFromCol[mapColIndex] = map
+      }
+    }
+
+    // Setup
+    const times = []
+    const players = []
+    const timeRegex = /^\d{2,3}:\d{2}\.\d{3}$/
+    const sheets = [
+      { playerCategory: 'Alien', name: 'PvM (Aliens)' },
+      { playerCategory: 'Player', name: 'PvM (Players)' },
+      { playerCategory: 'Player', name: 'PvM (Players 2)' },
+      { playerCategory: 'Player', name: 'PvM (Players 3)' },
+      { playerCategory: 'Novice', name: 'PvM (Novice)' },
+      { playerCategory: 'Novice', name: 'PvM (Novice 2)' },
+      { playerCategory: 'Novice', name: 'PvM (Novice 3)' },
+    ]
+    for (const sheet of sheets) {
+      const sheetJson = await fetchSheetData(sheet.name)
+      for (const [rowIndex, row] of sheetJson.table.rows.entries()) {
+        if (rowIndex < 6 || !row.c[1].v || row.c[1].v === '/') continue // Skip header or empty row
+        const playerId = uniqueStr()
+        const player = {
+          name: row.c[1].v,
+          category: sheet.playerCategory,
+          id: playerId,
+          rawFame: {}
+        }
+        players.push(player)
+        for (const [cellColIndex, cell] of row.c.entries()) {
+          if (cell && cell.v && cellColIndex > 1) {
+            const currentMap = mapsIdFromCol[cellColIndex]
+            if (!timeRegex.test(cell.v)) continue
+            const time = timeStrToNumber(cell.v)
+            const mapCategory = ['Alien', 'Player', 'Intermediate', 'Noob', 'Not defined'][
+              [currentMap.times.alien, currentMap.times.player, currentMap.times.intermediate, currentMap.times.noob]
+                  .findIndex(threshold => time < threshold) ?? 4
+            ]
+            times.push({
+              playerId,
+              mapId: currentMap.exchange.id,
+              time,
+              category: mapCategory
+            })
+          }
+        }
+      }
+    }
+
+    // Getting fame
+     // Will be translated on website from rawFame to fame
+    const fameJson = await fetchSheetData('Wall of Fame')
+    for (const [rowIndex, row] of fameJson.table.rows.entries()) {
+      if (!row.c[1] && !row.c[5]) continue
+      const playerLeft = row.c[1] ? row.c[1].v : '/'
+      const playerRight = row.c[5] ? row.c[5].v : '/'
+      if (playerLeft === '/' && playerRight === '/') continue
+
+      if (playerLeft !== '/') {
+        const findPlayerIndex = players.findIndex(p => p.name === playerLeft)
+        if (findPlayerIndex) {
+          players[findPlayerIndex].rawFame = {
+            player: row.c[2].v ?? undefined,
+            alien: row.c[3].v ?? undefined
+          }
+        }
+      }
+
+      if (playerRight !== '/') {
+        const findPlayerIndex = players.findIndex(p => p.name === playerRight)
+        if (findPlayerIndex) {
+          players[findPlayerIndex].rawFame = {
+            noob: row.c[6].v ?? undefined,
+            intermediate: row.c[7].v ?? undefined
+          }
+        }
+      }
+    }
+
+
+    writeData('times', times)
+    console.log(`Times generated successfully! - ${times.length} times`)
+    writeData('players', players)
+    console.log(`Players generated successfully! - ${players.length} players`)
+  } catch (error) {
+    console.error('Error generating Players:', error)
+  }
+}
 
 // Run the script
-generateMaps()
+// generateMaps()
+generatePlayers()
